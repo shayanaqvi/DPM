6507d34b925ce83760c48da25508a850ac9a9562
shayanaqvi <shayan.naqvi@icloud.com>
2023-07-08 14:47:53 +0500

    Cleaned up

--- Menu.py
+++ Menu.py
@@ -3,37 +3,20 @@ from client import client
 
 class Menu():
     """Handle menu generation"""
-
-    def __init__(self):
-        pass
-
     def generate_menu(self, prompts: list):
         """Generate a list of options for the user to choose from"""
-        # i = 1
         menu_array = []
         for item in prompts:
-            # print(f"{i}.", item)
             menu_array.append(f"{item}")
-            # i += 1
         return menu_array
 
     def list_directory(self, directory, type):
         """List a given directory"""
         list = client.lsinfo(directory)
         list_array = []
-        index = 0
 
         for item in list:
             list_array.append(item[type])
-
-        # for item in list_array:
-        #     if index < 9:
-        #         print(f"0{str(index+1)} {item}")
-        #         index += 1
-        #     else:
-        #         print(f"{str(index+1)} {item}")
-        #         index += 1
-
         return list_array
 
     def return_query_results(self, type, query):
@@ -47,11 +30,10 @@ class Menu():
             result = client.find(return_type[2], query)
         else:
             pass
-        # for item in result:
-        #     print(item[return_type[0]], item[return_type[1]], item[return_type[2]])
         return result
 
     def print_queue(self):
+        """Print items in current queue"""
         current_song = client.currentsong()
         current_playlist = client.playlistinfo()
         current_playlist_array = []
@@ -68,6 +50,3 @@ class Menu():
 
         for item in display_playlist_array[cs_index:]:
             print(item["artist"], item["album"], item["title"])
-
-    def temp(self):
-        print("h")
