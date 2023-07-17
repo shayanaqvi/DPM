f5bc4d405603edd7e6be705c79993db04eb57bf6
shayanaqvi <shayan.naqvi@icloud.com>
2023-07-17 11:55:40 +0500

    Appearance tweaks

--- info_panel.py
+++ info_panel.py
@@ -4,11 +4,12 @@ from rich.console import Console
 from Colours import colours
 
 
-def info_panel(information):
+def info_panel(information, colour):
     console = Console()
     panel = ""
     panel = Panel(
         information,
-        style=colours["red"],
+        style=colours[colour],
+        box=box.SIMPLE_HEAD
     )
     console.print(panel)
