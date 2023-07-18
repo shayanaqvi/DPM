429bfc876f9635a765923519b501c02a32e29e19
shayanaqvi <shayan.naqvi@icloud.com>
2023-07-18 21:41:37 +0500

    Return the name of the selected artist

--- browse.py
+++ browse.py
@@ -35,17 +35,59 @@ def generate_table(columns: list):
 
 def populate_table(table_to_populate, population: list):
     table = table_to_populate
+
+    # loop through the items in population[], add them to the table
     for i, populant in enumerate(population):
         table.add_row(
-            str(i+1),
+            str(i+1),  # add indexes to the table
             populant
         )
 
     return table
 
 
-def handle_input():
-    pass
+def handle_input(user_input, list_of_media):
+    global current_level
+
+    # split the user's input into an array
+    # to make parts of it easier to access
+    user_input_array = []
+    for item in user_input.split(" "):
+        user_input_array.append(item)
+
+
+    # this will check if only an integer has been input,
+    # or whether a letter prepends it
+    match len(user_input_array):
+        # equivalent to viewing a subdirectory
+        case 1:
+            try:
+                # use isinstance()?
+                # check if the input is an integer or not
+                if user_input_array[0].isdigit():
+                    # this operation is not supported at level 6
+                    if current_level == 6:
+                        pass
+                    else:
+                        # return the item at the index selected by the user
+                        selection = list_of_media[int(user_input_array[0]) - 1]
+                        current_level += 1
+                        return selection
+
+                # if 'a' was input with no index
+                elif user_input_array[0] == "a":
+                    console.print("No index was supplied")
+
+            # if the user selects an invalid index
+            except (IndexError):
+                console.print("This index does not exist")
+
+        # add an item to the queue
+        case 2:
+            pass
+        # invalid input
+        case _:
+            pass
 
 
 def process_output(output, type_of_media):
@@ -90,8 +132,8 @@ def main():
                 # Level 2: get input form the user
                 try:
                     user_input_unprocessed = input(": ")  # unprocessed user input
-                    user_input = handle_input()  # have the user's input processed
-                    break
+                    user_input = handle_input(user_input_unprocessed, artists)  # have the user's input processed
+                    console.print(user_input)
                 except (KeyboardInterrupt, EOFError):
                     cs()
                     return
