cb5d9f92e443c90b8e0375740cf3ef693819ed66
shayanaqvi <shayan.naqvi@icloud.com>
2023-07-20 19:36:28 +0500

    Added a dictionary of messages to communicate with the user

---
+++ messages.py
@@ -0,0 +1,6 @@
+messages = {
+    "Invalid option": "This operation is not supported here",
+    "No index": "No index was supplied",
+    "Invalid index": "⚠ This index does not exist",
+    "Added to queue": "✓ Selection added to queue"
+}
